15195fdb0bef09230bf417821b5f107d0e7c008e
Bria <38300358+yungbreezei@users.noreply.github.com>
2025-04-27 13:13:56 -0500

    Delete module_4/assignment_4 directory

--- module_4/assignment_4/src/ict4315/parking/charges/factory/ParkingChargeStrategyFactory.java
+++
@@ -1,9 +0,0 @@
-package ict4315.parking.charges.factory;
-
-import ict4315.parking.charges.strategy.ParkingChargeStrategy;
-import ict4315_assignment_1.ParkingLot;
-
-public interface ParkingChargeStrategyFactory {
-
-	ParkingChargeStrategy getStrategyFor(ParkingLot lot);
-}
--- module_4/assignment_4/src/ict4315/parking/charges/strategy/package-info.java
+++
@@ -1,1 +0,0 @@
-package ict4315.parking.charges.strategy;
